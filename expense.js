4f94866d15a9e7907393f502fcdf31f8b7c8eb5c
fahadshoukat <fraza6970@gmail.com>
2022-01-05 22:10:46 +0500

    addFunctionalityToBalanceIncomeAndExpenseElements

--- expense.js
+++ expense.js
@@ -1,4 +1,7 @@
 let transactionItems = [];
+let totalBalance = 0;
+let totalIncome = 0;
+let totalExpense = 0;
 $("#addBtn").click(function (e) {
   e.preventDefault();
 
@@ -7,13 +10,34 @@ $("#addBtn").click(function (e) {
 
   transactionItems.push({ transactionText, transactionAmount });
 
- $(".transaction-list").html(transactionItems.map((item) => {
-     if(item.transactionAmount > 0){
-        $("li").css({"border-right:"})
-     }
-    return `<li>
+  if (transactionAmount > 0) {
+
+    totalIncome += +transactionAmount;
+    totalBalance += +transactionAmount;
+
+} 
+  else {
+  
+    totalExpense += +transactionAmount;
+    totalBalance += +transactionAmount;
+  
+}
+
+  $(".balance").html("$" + totalBalance + ".00");
+  $(".green").html("$" + totalIncome + ".00");
+  $(".red").html("$" + totalExpense + ".00");
+
+  $(".transaction-list").html(
+    transactionItems.map((item) => {
+      if (item.transactionAmount > 0) {
+        $("li").addClass("green-border");
+      } else {
+        $("li").addClass("red-border");
+      }
+      return `<li>
         <span>${item.transactionText}</span>
-        <span>${item.transactionAmount}</span>
+        <span>$${item.transactionAmount}</span>
     </li>`;
-  }));
+    })
+  );
 });
