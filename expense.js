7b892cf9a4f6bf87f09984ff314d968da724f173
fahadshoukat <fraza6970@gmail.com>
2022-01-08 23:00:53 +0500

    createOneObjectAndFunction

--- expense.js
+++ expense.js
@@ -1,55 +1,60 @@
-let transactionItems = [];
-let totalBalance = 0;
-let totalIncome = 0;
-let totalExpense = 0;
+let myObj = {
+  transactionItems: [],
+  totalBalance: 0,
+  totalIncome: 0,
+  totalExpense: 0,
+};
+
 $("#addBtn").click(function (e) {
   e.preventDefault();
 
   let transactionText = $("#text").val();
   let transactionAmount = $("#amount").val();
 
-  transactionItems.push({ transactionText, transactionAmount });
+  myObj.transactionItems.push({ transactionText, transactionAmount });
 
   if (transactionAmount > 0) {
+    myObj.totalIncome += +transactionAmount;
+    myObj.totalBalance += +transactionAmount;
+  } else {
+    myObj.totalExpense += +transactionAmount;
+    myObj.totalBalance += +transactionAmount;
+  }
+  displayList();
 
-    totalIncome += +transactionAmount;
-    totalBalance += +transactionAmount;
+  $("#text").val("");
+  $("#amount").val(null);
+});
 
-} 
-  else {
-  
-    totalExpense += +transactionAmount;
-    totalBalance += +transactionAmount;
-  
-}
+function displayList() {
+  $(".balance").html("$" + myObj.totalBalance + ".00");
+  $(".green").html("$" + myObj.totalIncome + ".00");
+  $(".red").html("$" + myObj.totalExpense + ".00");
 
-  $(".balance").html("$" + totalBalance + ".00");
-  $(".green").html("$" + totalIncome + ".00");
-  $(".red").html("$" + totalExpense + ".00");
-
-  $('.transaction-list').html(() => {
-    let myHtml = ''
-    transactionItems.forEach((item) => {
+  $(".transaction-list").html(() => {
+    let myHtml = "";
+    myObj.transactionItems.forEach((item) => {
       if (item.transactionAmount > 0) {
         myHtml += `<li class='green-border'>
         <div class="deleteBtn">X</div>
       <span>${item.transactionText}</span>
       <span>$${item.transactionAmount}</span>
-  </li>`
+  </li>`;
       } else {
         myHtml += `<li class='red-border'>
         <div class="deleteBtn">X</div>
         <span>${item.transactionText}</span>
         <span>$${item.transactionAmount}</span>
-    </li>`
+    </li>`;
       }
-    })
-   
-    return myHtml
-    
-  })
+    });
 
-  $('#text').val('');
-  $('#amount').val(null);
+    return myHtml;
+  });
+}
 
+$(".transaction-list").click((e) => {
+  let removeItem = e.target.parentNode;
+  
+  myObj.transactionItems.splice(removeItem, 1);
 });
